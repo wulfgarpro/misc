f3845252cee95f044217cd4fa571d2eab2590783
James Fraser <wulfgar.pro@gmail.com>
2012-12-27 23:00:17 +1030

    updated basic specs

--- samples/express-jade/test/spec/simple.spec.js
+++ samples/express-jade/test/spec/simple.spec.js
@@ -1,5 +1,6 @@
 describe('A simple test suite', function() {
     var val = true;
+
     it('val should be true', function() {
         expect(val).toBe(true);
     });
---
+++ samples/express-jade/test/spec/snippet.spec.js
@@ -0,0 +1,10 @@
+describe('A snippet', function() {
+    it('can be added', function() {
+    });
+
+    it('can be retrieved', function() {
+    });
+
+    it('can be removed', function() {
+    });
+});
